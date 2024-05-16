57aec39b75d83e259787ad48d7c6df03e35af6a5
ameridass <ameridas1998@gmail.com>
2024-05-16 00:48:15 -0600

    feat
    inicio de sesión para clientes.

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -1,19 +1,63 @@
-import React, {useState} from 'react';
-import {View, TextInput, Button, StyleSheet, Text} from 'react-native';
+import React, { useState } from 'react';
+import { View, TextInput, Button, StyleSheet, Text } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {API} from "../constants/Api";
 
 // @ts-ignore
-const LoginScreen = ({navigation}) => {
+const LoginScreen = ({ navigation }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [userType, setUserType] = useState('user'); // 'user' or 'collaborator'
+  const [error, setError] = useState('');
 
-  const handleLogin = () => {
-    console.log('Login with:', email, password);
-    navigation.navigate('Main');
+  const handleLogin = async () => {
+    let endpoint = '';
+    if (userType === 'user') {
+      endpoint = API.endpoint.duenos+'login/';
+    } else if (userType === 'collaborator') {
+      endpoint = 'API_ENDPOINT_FOR_COLLABORATOR';
+    }
+
+    try {
+      const response = await fetch(API.url_dev + endpoint, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      const data = await response.json();
+
+      if (data) {
+        console.log('Login successful:', data.mensaje);
+        // Store user type and user ID in AsyncStorage
+        await AsyncStorage.setItem('userType', userType);
+        await AsyncStorage.setItem('userId', data.id.toString());
+
+        navigation.navigate('Main');
+      } else {
+        setError('Usuario o contraseña no válidos');
+      }
+    } catch (error) {
+      setError('Error al conectarse al servidor');
+      console.error('Login error:', error);
+    }
   };
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Iniciar Sesión</Text>
+      {error ? <Text style={styles.error}>{error}</Text> : null}
+      <Picker
+        selectedValue={userType}
+        onValueChange={(itemValue) => setUserType(itemValue)}
+        style={styles.picker}
+      >
+        <Picker.Item label="Usuario" value="user" />
+        <Picker.Item label="Colaborador" value="collaborator" />
+      </Picker>
       <TextInput
         style={styles.input}
         placeholder="Correo electrónico"
@@ -54,6 +98,16 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: '#ddd',
     borderRadius: 5,
+  },
+  picker: {
+    height: 50,
+    width: '100%',
+    marginBottom: 10,
+  },
+  error: {
+    color: 'red',
+    marginBottom: 10,
+    textAlign: 'center',
   },
 });
 
