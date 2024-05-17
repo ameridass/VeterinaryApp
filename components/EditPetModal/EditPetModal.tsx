0ca9c488f870e7969895064179d32da587b0f408
ameridass <ameridas1998@gmail.com>
2024-05-16 18:46:45 -0600

    feat
    Modal funcional al editar

--- components/EditPetModal/EditPetModal.tsx
+++ components/EditPetModal/EditPetModal.tsx
@@ -1,18 +1,22 @@
 // EditPetModal.tsx
 import React, { useEffect, useState } from 'react';
-import {View, Text, TextInput, TouchableOpacity, StyleSheet, Modal, ActivityIndicator, Alert} from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Modal, ActivityIndicator, Alert } from 'react-native';
 import {API} from "../../constants/Api";
-import {Pet} from "../../constants/Types";
-
 
 // @ts-ignore
-const EditPetModal = ({ visible, onClose, petId }) => {
-  const [petDetails, setPetDetails] = useState<Pet>(null);
+const EditPetModal = ({ visible, onClose, petId, onSave }) => {
+  const [petDetails, setPetDetails] = useState({
+    nombre: '',
+    especie: '',
+    raza: '',
+    fecha_nacimiento: '',
+    historial_medico: ''
+  });
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const fetchPetDetails = async () => {
-      if (petId) {
+    if (petId) {
+      const fetchPetDetails = async () => {
         try {
           const response = await fetch(`${API.url_dev}${API.endpoint.mascotas}${petId}`);
           const data = await response.json();
@@ -22,11 +26,41 @@ const EditPetModal = ({ visible, onClose, petId }) => {
         } finally {
           setLoading(false);
         }
-      }
-    };
+      };
 
-    fetchPetDetails();
+      fetchPetDetails();
+    } else {
+      setLoading(false);
+    }
   }, [petId]);
+
+  const handleSave = async () => {
+    try {
+      const method = petId ? 'PUT' : 'POST';
+      const url = petId
+        ? `${API.url_dev}${API.endpoint.mascotas}${petId}/`
+        : `${API.url_dev}${API.endpoint.mascotas}`;
+      console.log('url:', url);
+      const response = await fetch(url, {
+        method: method,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(petDetails),
+      });
+
+      if (response.ok) {
+        Alert.alert('Exito', 'La mascota fue agregada correctamente!');
+        onSave();
+        onClose();
+      } else {
+        Alert.alert('Notificación', 'Ha fallado la operación, por favor intente de nuevo más tarde');
+      }
+    } catch (error) {
+      console.error('Error saving pet details:', error);
+      Alert.alert('Error', 'Ha ocurrido un error al guardar los detalles de la mascota, por favor intente de nuevo más tarde');
+    }
+  };
 
   if (loading) {
     return (
@@ -38,39 +72,11 @@ const EditPetModal = ({ visible, onClose, petId }) => {
     );
   }
 
-  if (!petDetails) {
-    return null;
-  }
-
-  const handleSave = async () => {
-    if (petDetails) {
-      try {
-        const response = await fetch(`${API.url_dev}${API.endpoint.mascotas}${petDetails.id}/`, {
-          method: 'PUT',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(petDetails),
-        });
-
-        if (response.ok) {
-          Alert.alert('Success', 'Información de mascota actualizado correctamente!');
-          onClose();
-        } else {
-          Alert.alert('Error', 'Ha fallado la actualización de la información de mascota');
-        }
-      } catch (error) {
-        console.error('Error updating pet details:', error);
-        Alert.alert('Error', 'Ha fallado la actualización de la información de mascota, intenta de nuevo más tarde');
-      }
-    }
-  };
-
   return (
     <Modal visible={visible} transparent={true}>
       <View style={styles.modalContainer}>
         <View style={styles.modalContent}>
-          <Text style={styles.modalTitle}>Información de mascota</Text>
+          <Text style={styles.modalTitle}>{petId ? 'Edit Pet Details' : 'Add New Pet'}</Text>
           <TextInput
             style={styles.input}
             value={petDetails.nombre}
@@ -102,7 +108,7 @@ const EditPetModal = ({ visible, onClose, petId }) => {
             placeholder="Historial Médico"
           />
           <TouchableOpacity style={styles.saveButton} onPress={handleSave}>
-            <Text style={styles.saveButtonText}>Guardar cambios</Text>
+            <Text style={styles.saveButtonText}>{petId ? 'Guardar' : 'Agregar'}</Text>
           </TouchableOpacity>
           <TouchableOpacity style={styles.closeButton} onPress={onClose}>
             <Text style={styles.closeButtonText}>Cancelar</Text>
